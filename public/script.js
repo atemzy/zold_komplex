bd3b08787152da79047bc65b08cde1498ef7f403
Atemzy <43964339+atemzy@users.noreply.github.com>
2023-06-02 22:45:23 +0200

    szopás

--- public/script.js
+++ public/script.js
@@ -49,6 +49,58 @@ new ScrollMagic.Scene({
     indent: 0
   })
   .addTo(controller);
-window.onresize = function (event) {
+/*window.onresize = function (event) {
   location.reload();
-};
+};*/
+let activated = []
+let currentSlider = 0
+let realSlider = 0
+let direction = 1
+let prevScroll = 0
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  if (scroll > prevScroll) {
+    direction = 1
+  } else if (scroll < prevScroll) {
+    direction = -1
+  }
+  prevScroll = scroll
+  currentSlider = Math.floor(scroll / window.innerHeight + 1);
+  realSlider = scroll / window.innerHeight + 1
+
+  setActiveDesc(realSlider.toFixed(1))
+});
+function setActiveDesc(num) {
+  let elements = 0
+  console.log(direction)
+  if (num < 10) {
+    if (direction == 1) {
+      elements = document.getElementsByClassName("a0" + (num - 0.5))
+    } else {
+      elements = document.getElementsByClassName("a0" + (num - 1))
+    }
+  } else {
+    if (direction == 1) {
+      elements = document.getElementsByClassName("a" + (num - 0.5))
+    } else {
+      elements = document.getElementsByClassName("a" + (num - 1))
+    }
+  }
+  if (elements.length != 0) torles()
+  for (let e = 0; e < elements.length; e++) {
+    let children = elements[e].children
+    for (let o = 0; o < children.length; o++) {
+      if (children[o].classList.contains("desc")) {
+        children[o].classList.add("active")
+        activated.push(children[o])
+        console.log("hozzáad")
+      }
+    }
+  }
+}
+function torles() {
+  for (let b = 0; b < activated.length; b++) {
+    activated[b].classList.remove("active")
+    console.log("elvesz")
+  }
+}
